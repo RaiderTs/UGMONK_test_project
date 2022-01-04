8c891432e526f442849771d9a97cd22aff7db437
webPandaren <63519514+RaiderTs@users.noreply.github.com>
2022-01-04 15:33:45 +0200

    add button to basket

--- src/components/ProductDetailsPage/ProductDetailsPage.tsx
+++ src/components/ProductDetailsPage/ProductDetailsPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'antd/dist/antd.css';
-import { Modal } from 'antd';
+import { Modal, Button } from 'antd';
+import { ShoppingCartOutlined } from '@ant-design/icons';
 import styles from './ProductDetailsPage.module.css';
 import { IProduct } from '../MainPage/components/CardComponent/CardComponent';
 import '../../css/variablesAnt.css';
@@ -26,18 +27,29 @@ const ProductDetailsPage: React.FC<ModalProps> = ({
         visible={isModalVisible}
         onOk={handleOk}
         onCancel={handleCancel}
+        okButtonProps={{ style: { display: 'none' } }}
       >
-        <div>
+        <div style={{ position: 'relative' }}>
           <p className={styles.detail__name}>{product.name}</p>
           <div className={styles.detail__container}>
             <p>
-              <img src={product.image} alt="" />
+              <img src={product.image} alt="img" width="400" height="500" />
             </p>
             <div>
               <p className={styles.detail__productDescription}>
                 Product description
               </p>
               <p className={styles.detail__description}>{product.text}</p>
+              <div className={styles.detail__buttonAddToBasket}>
+                <Button
+                  type="primary"
+                  icon={<ShoppingCartOutlined style={{ fontSize: '20px' }} />}
+                  size="large"
+                  style={{ backgroundColor: '#000000', border: 'none' }}
+                >
+                  Add to basket
+                </Button>
+              </div>
             </div>
           </div>
         </div>
